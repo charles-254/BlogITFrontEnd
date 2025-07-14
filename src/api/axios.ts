6ad6fc2ebc117414f2ee72b709b15ccfbb1ca9b8
charles-254 <kamaucharles6884@gmailcom>
2025-07-14 21:18:41 +0300

    update axiosInstance baseUrl

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -10,8 +10,8 @@ import axios from "axios";
 // import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://blogitbackend-1.onrender.com",
-  baseURL: "http://127.0.0.1:3000",
+  baseURL: "https://blogitbackend-1.onrender.com",
+  
 });
 
 axiosInstance.interceptors.request.use(
